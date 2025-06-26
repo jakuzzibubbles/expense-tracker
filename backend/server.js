d8616194ac026c7615486508b81f5bf2b866dfc3
jakuzzibubbles <jakuzzi263@gmail.com>
2025-06-27 00:09:48 +0200

    connected to mongodb and implenent authRoutes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -9,12 +11,19 @@ const app = express();
 app.use(
     cors({
         origin: process.env.CLIENT_URL || "*", 
-        methods: ["GET", "POST", "GET", "DELETE"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
 
 app.use(express.json());
+
+connectDB();
+
+app.use("/api/v1/auth", authRoutes);
+
+// serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
