bbdb2fef9dabd2b80422376b8b80634cabf65e89
jakuzzibubbles <jakuzzi263@gmail.com>
2025-06-27 23:26:48 +0200

    Refactor authentication components and add UserContext
    - Modified App.jsx to integrate context and improve structure
    - Added UserContext.jsx for managing user state globally
    - Updated Login.jsx and SignUp.jsx for better authentication flow

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -3,6 +3,10 @@ import AuthLayout from '../../components/layouts/AuthLayout';
 import { useNavigate, Link } from 'react-router-dom';
 import Input from '../../components/Inputs/Input';
 import { validateEmail } from '../../utils/helper';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { useContext } from 'react';
+import { UserContext } from '../../context/userContext';
 
 
 const Login = () => {
@@ -10,10 +14,12 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
+  const { updateUser } = useContext(UserContext);
+
   const navigate = useNavigate();
 
   // Handle Login form submit
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
 
     if (!validateEmail(email)) {
@@ -29,7 +35,27 @@ const Login = () => {
     setError("");
 
     // Login API call
+    try {
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      });
+      const { token, user } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(user);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
+    }
   }
+
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
