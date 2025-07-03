bd966ccb9fb3abca86dbf1001a75f0f5bb7e42bf
jakuzzibubbles <jakuzzi263@gmail.com>
2025-07-03 17:14:07 +0200

    added CustomBarChart component

--- frontend/expense-tracker/src/components/Charts/CustomBarChart.jsx
+++ frontend/expense-tracker/src/components/Charts/CustomBarChart.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Cell } from 'recharts';
 
-const CustomBarChart = ({ data }) => {
+
+const CustomBarChart = ({data}) => {
   return (
     <div>CustomBarChart</div>
   )
